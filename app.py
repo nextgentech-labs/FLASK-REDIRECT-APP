3a6e45bde4ad5f3db69928ebb3ddf540ffb4eb0c
nextgentech-labs <pyhacker39@gmail.com>
2026-06-19 22:59:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Replace this with the internal IP or URL you want to redirect to
+    return redirect('http://127.0.0.1', code=302)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
