e8ad08a9f7f7fddac838038935d87054a73078e7
nextgentech-labs <pyhacker39@gmail.com>
2026-06-20 10:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Replace this with the internal IP or URL you want to redirect to
-    return redirect('http://169.254.169.254', code=302)
+    return redirect('http://1bd7khvodggb02gaydhcwzvu7ldc12pr.oastify.com', code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
